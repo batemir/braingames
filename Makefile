e82ee5b7fffe45d1817cb53a9313461a46912e4a
Batemir <vadikkarcev666@gmail.com>
2018-01-25 16:50:57 +0300

    add brain-gcd

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ start-even:
 	npm run babel-node -- src/bin/brain-even.js
 start-calc:
 	npm run babel-node -- src/bin/brain-calc.js
+start-gcd:
+	npm run babel-node -- src/bin/brain-gcd.js
 publish:
 	npm publish
 lint: 
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,25 @@
+import runGame from '../engine';
+import { getRandomNumber } from '..';
+
+const solver = () => {
+  const first = getRandomNumber(100);
+  const second = getRandomNumber(100);
+  let i;
+  const question = `${first} ${second}`;
+  let answer;
+  if (first > second || first === second) {
+    i = second;
+  } else {
+    i = first;
+  }
+  for (i; i > 0; i -= 1) {
+    if (first % i === 0 && second % i === 0) {
+      answer = i;
+      break;
+    }
+  }
+  return [question, answer];
+};
+const textOfTask = 'Find the greatest common divisor of given numbers.';
+const brainGcd = () => runGame(textOfTask, 3, solver);
+export default brainGcd;
