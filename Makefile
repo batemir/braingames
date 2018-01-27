259551fd7c8faf692257a444425554aba7144403
Batemir <vadikkarcev666@gmail.com>
2018-01-27 22:50:57 +0300

    add brain-progression

--- Makefile
+++ Makefile
@@ -10,6 +10,8 @@ start-gcd:
 	npm run babel-node -- src/bin/brain-gcd.js
 start-balance:
 	npm run babel-node -- src/bin/brain-balance.js
+start-progression:
+	npm run babel-node -- src/bin/brain-progression.js
 publish:
 	npm publish
 lint: 
---
+++ src/games/brain-progression.js
@@ -0,0 +1,24 @@
+import runGame from '../engine';
+import { getRandomNumber } from '..';
+
+const getTask = () => {
+  const firstInSequence = getRandomNumber(100);
+  const step = getRandomNumber(9, 2);
+  const missingStep = getRandomNumber(8);
+  const sequence = [firstInSequence];
+  for (let i = 0; i < 9; i += 1) {
+    if (i === missingStep) {
+      sequence.push('..');
+    } else if (i - 1 === missingStep) {
+      sequence.push(sequence[i - 1] + (step * 2));
+    } else {
+      sequence.push(sequence[i] + step);
+    }
+  }
+  const answer = sequence[missingStep] + step;
+  const question = sequence.join(' ');
+  return [question, answer];
+};
+const textOfTask = 'What number is missing in this progression?';
+const brainProgression = runGame(textOfTask, getTask);
+export default brainProgression;
